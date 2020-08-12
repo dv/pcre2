68d5e0b2b687024be2f9518982ee41e2d55cdf82
David Verhasselt <david@crowdway.com>
2020-08-12 08:45:27 +0300

    Unnest class for cleaner namespace

--- lib/pcre2/regexp.rb
+++ lib/pcre2/regexp.rb
@@ -1,35 +1,37 @@
-class PCRE2::Regexp
-  attr :source, :pattern_ptr
+module PCRE2
+  class Regexp
+    attr :source, :pattern_ptr
 
-  def initialize(pattern, *options)
-    @source = pattern
-    @pattern_ptr = PCRE2::Lib.compile_pattern(pattern, options)
-  end
-
-  # Compiles the Regexp into a JIT optimised version. Returns whether it was successful
-  def jit!
-    options = PCRE2::PCRE2_JIT_COMPLETE | PCRE2::PCRE2_JIT_PARTIAL_SOFT | PCRE2::PCRE2_JIT_PARTIAL_HARD
-
-    PCRE2::Lib.pcre2_jit_compile_8(pattern_ptr, options) == 0
-  end
-
-  def match(str, pos = nil)
-    result_count, match_data_ptr = PCRE2::Lib.match(@pattern_ptr, str, position: pos)
-
-    if result_count == 0
-      nil
-    else
-      pairs = PCRE2::Lib.get_ovector_pairs(match_data_ptr, result_count)
-
-      PCRE2::MatchData.new(self, str, pairs)
+    def initialize(pattern, *options)
+      @source = pattern
+      @pattern_ptr = Lib.compile_pattern(pattern, options)
     end
-  end
 
-  def named_captures
-    @named_captures ||= PCRE2::Lib.named_captures(pattern_ptr)
-  end
+    # Compiles the Regexp into a JIT optimised version. Returns whether it was successful
+    def jit!
+      options = PCRE2_JIT_COMPLETE | PCRE2_JIT_PARTIAL_SOFT | PCRE2_JIT_PARTIAL_HARD
 
-  def names
-    named_captures.keys
+      Lib.pcre2_jit_compile_8(pattern_ptr, options) == 0
+    end
+
+    def match(str, pos = nil)
+      result_count, match_data_ptr = Lib.match(@pattern_ptr, str, position: pos)
+
+      if result_count == 0
+        nil
+      else
+        pairs = PCRE2::Lib.get_ovector_pairs(match_data_ptr, result_count)
+
+        MatchData.new(self, str, pairs)
+      end
+    end
+
+    def named_captures
+      @named_captures ||= Lib.named_captures(pattern_ptr)
+    end
+
+    def names
+      named_captures.keys
+    end
   end
 end
