811cf7ce42ff79ad1cb7263d95d2c209af168e61
David Verhasselt <david@crowdway.com>
2020-08-15 11:53:39 +0300

    Fix #split to accurately mirror String#split

--- lib/pcre2/string_utils.rb
+++ lib/pcre2/string_utils.rb
@@ -15,18 +15,29 @@ module PCRE2::StringUtils
     return enum_for(:split, string).to_a if !block_given?
 
     previous_position = 0
-    matches(string).each do |matchdata|
+    matches(string) do |matchdata|
       beginning, ending = matchdata.offset(0)
+
+      # If zero-length match and the previous_position is equal to the match position, just skip
+      # it. The next zero-length match will have a different previous_position and generate a split
+      # which results in the appearance of a "per character split" but without empty parts in the
+      # beginning. Note that we're also skipping adding capture groups.
+      if matchdata.length == 0 && previous_position == beginning
+        next
+      end
 
       yield string[previous_position ... beginning]
 
-      if matchdata.captures.any?
-        matchdata.captures.each do |capture|
-          yield capture
-        end
+      matchdata.captures.each do |capture|
+        yield capture
       end
 
       previous_position = ending
+    end
+
+    # Also return the ending of the string from the last match
+    if previous_position < string.length
+      yield string[previous_position .. -1]
     end
   end
 end
--- spec/lib/pcre2/string_utils_spec.rb
+++ spec/lib/pcre2/string_utils_spec.rb
@@ -30,22 +30,77 @@ RSpec.describe PCRE2::StringUtils do
   end
 
   describe "#split" do
+    let(:string) {  "and a 1 and a 2 and a 345 congrats!" }
+
     it "splits where the regexp matches" do
-      subject = "and a 1 and a 2 and a 345"
       regexp = PCRE2::Regexp.new('\d+')
 
-      result = regexp.split(subject)
+      result = regexp.split(string)
 
-      expect(result).to eq(["and a ", " and a ", " and a "])
+      expect(result).to eq(["and a ", " and a ", " and a ", " congrats!"])
     end
 
     it "returns captures in the result too" do
-      subject = "and a 1 and a 12 and a 345"
       regexp = PCRE2::Regexp.new('\d(\d*)')
 
-      result = regexp.split(subject)
+      result = regexp.split(string)
 
-      ["and a ", "", " and a ", "2", " and a ", "45"]
+      expect(result).to eq(["and a ", "", " and a ", "", " and a ", "45", " congrats!"])
+    end
+
+    it "splits each character for zero-length matches" do
+      regexp = PCRE2::Regexp.new('')
+
+      result = regexp.split(string)
+
+      expect(result).to eq(string.chars)
+    end
+
+    # These are tests that were reverse-engineered from what String#split returns, since the documentation
+    # or the original source code is not very clear about what the exact specification of `split` should be
+    # when there are zero-length matches.
+    context "edge-cases" do
+      let(:string) { "abcde" }
+
+      it "has an empty part at the beginning" do
+        regexp = PCRE2::Regexp.new("a")
+
+        result = regexp.split(string)
+
+        expect(result).to eq(["", "bcde"])
+      end
+
+      it "has a zero-length match so split by characters without empty part at the beginning" do
+        regexp = PCRE2::Regexp.new("")
+
+        result = regexp.split(string)
+
+        expect(result).to eq(["a", "b", "c", "d", "e"])
+      end
+
+      it "has a zero-length match in the middle so split into two parts" do
+        regexp = PCRE2::Regexp.new("(?=c)")
+
+        result = regexp.split(string)
+
+        expect(result).to eq(["ab", "cde"])
+      end
+
+      it "has a zero-length match at the start and in the middle but again only split into two parts without empty part at the beginning" do
+        regexp = PCRE2::Regexp.new("^|(?=c)")
+
+        result = regexp.split(string)
+
+        expect(result).to eq(["ab", "cde"])
+      end
+
+      it "has multiple zero-length matches including empty capture groups so split by chars and also include lots of empty results" do
+        regexp = PCRE2::Regexp.new("()|^|(?=c)")
+
+        result = regexp.split(string)
+
+        expect(result).to eq(["a", "", "b", "", "c", "", "d", "", "e"])
+      end
     end
   end
 end
